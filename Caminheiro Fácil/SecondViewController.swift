33ff7be48478ac2aecb361945eaa3da169d45655
Rodrigo Gomes Dias <tqi_rdias@uolinc.com>
2020-06-13 01:00:50 -0300

    Initial Commit

---
+++ Caminheiro Fácil/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Caminheiro Fácil
+//
+//  Created by Rodrigo Gomes Dias on 13/06/20.
+//  Copyright © 2020 CCR. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

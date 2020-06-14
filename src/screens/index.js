11ad9d657e58607044cd26f418058db32267c08e
Andrey Suprano <andsuprano@gmail.com>
2020-06-14 16:10:25 -0300

    Add ghpages

--- src/screens/index.js
+++
@@ -1,8 +0,0 @@
-import HomeScreen from "./HomeScreen";
-import CadastroScreen from "./CadastroScreen";
-import Saude from "./Times/saude";
-import Familia from "./Times/familia";
-import Mapa from "./Times/mapa";
-import Mulher from "./Times/mulher";
-
-export {HomeScreen, CadastroScreen, Saude, Familia, Mapa, Mulher};
